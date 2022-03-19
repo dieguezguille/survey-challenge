792397e4d70be907eef5befb4e58519242fb0d8f
Guillermo Diéguez <guillermodieguez@outlook.com>
2022-03-19 16:43:49 -0300

    ✨ add get balance functionality

--- src/components/context/quiz.context.tsx
+++ src/components/context/quiz.context.tsx
@@ -1,10 +1,95 @@
-import React, { createContext } from 'react';
+import { Contract, ethers } from 'ethers';
+import { useSnackbar } from 'notistack';
+import React, {
+    createContext,
+    useCallback,
+    useContext,
+    useEffect,
+    useState,
+} from 'react';
+import contractAbi from '../../abis/survey.json';
+import { AppContext } from './app.context';
+import { WalletContext } from './wallet.context';
 
-const defaultValues = {};
+type QuizContextProps = {
+    balance: number;
+    getBalance: () => Promise<void>;
+};
+
+const defaultValues: QuizContextProps = {
+    balance: 0,
+    getBalance: async () => {},
+};
+
 export const QuizContext = createContext(defaultValues);
 
-const QuizContextProvider: React.FC = ({ children }) => (
-    <QuizContext.Provider value={{}}>{children}</QuizContext.Provider>
-);
+const QuizContextProvider: React.FC = ({ children }) => {
+    const { setIsLoading } = useContext(AppContext);
+    const { address, provider } = useContext(WalletContext);
+    const { enqueueSnackbar } = useSnackbar();
+    const [balance, setBalance] = useState<number>(defaultValues.balance);
+    const [contract, setContract] = useState<Contract | undefined>();
+
+    const getBalance = async () => {
+        setIsLoading(true);
+        try {
+            if (contract && address) {
+                const result = await contract.balanceOf(address);
+                const newBalance = await ethers.utils.formatUnits(result, 18);
+                setBalance(parseFloat(newBalance));
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    const loadContract = useCallback(async () => {
+        setIsLoading(true);
+        try {
+            const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
+            if (contractAddress) {
+                setContract(
+                    await new ethers.Contract(
+                        contractAddress,
+                        contractAbi,
+                        provider?.getSigner()
+                    )
+                );
+            } else {
+                enqueueSnackbar('No contract address detected.', {
+                    variant: 'error',
+                });
+            }
+        } catch (error) {
+            enqueueSnackbar('Load contract operation failed.', {
+                variant: 'error',
+            });
+            console.log(error);
+        } finally {
+            setIsLoading(false);
+        }
+    }, [enqueueSnackbar, provider, setIsLoading]);
+
+    useEffect(() => {
+        if (!contract) {
+            loadContract();
+        }
+    }, [contract, loadContract]);
+
+    const contextValue = {
+        balance,
+        getBalance,
+        setBalance,
+        contract,
+    };
+
+    return (
+        <QuizContext.Provider value={contextValue}>
+            {children}
+        </QuizContext.Provider>
+    );
+};
 
 export default QuizContextProvider;
--- src/components/views/main/main.view.tsx
+++ src/components/views/main/main.view.tsx
@@ -1,9 +1,21 @@
+import { Stack } from '@mui/material';
 import Container from '@mui/material/Container';
+import { useContext, useEffect } from 'react';
+import { QuizContext } from '../../context/quiz.context';
 
 const MainView: React.FC = () => {
+    const { balance, getBalance } = useContext(QuizContext);
+
+    useEffect(() => {
+        getBalance();
+    }, [getBalance]);
+
     return (
         <Container>
-            <h2>Welcome.</h2>
+            <Stack>
+                <h1>Welcome!</h1>
+                <h2>Your current balance is: {balance} $QUIZ</h2>
+            </Stack>
         </Container>
     );
 };
