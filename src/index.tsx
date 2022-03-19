7effcd7a4934ed469106906ef17b99a27530ec1d
Guillermo Diéguez <guillermodieguez@outlook.com>
2022-03-19 00:25:38 -0300

    ✨ add context providers to app

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,23 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import CustomTheme from './theme/custom.theme';
 import App from './App';
 import { SnackbarProvider } from 'notistack';
+import AppContextProvider from './components/context/app.context';
+import WalletContextProvider from './components/context/wallet.context';
 
 ReactDOM.render(
     <React.StrictMode>
-        <ThemeProvider theme={CustomTheme}>
-            <SnackbarProvider maxSnack={5}>
-                <BrowserRouter>
-                    <CssBaseline />
-                    <App />
-                </BrowserRouter>
-            </SnackbarProvider>
-        </ThemeProvider>
+        <AppContextProvider>
+            <WalletContextProvider>
+                <ThemeProvider theme={CustomTheme}>
+                    <SnackbarProvider maxSnack={5}>
+                        <BrowserRouter>
+                            <CssBaseline />
+                            <App />
+                        </BrowserRouter>
+                    </SnackbarProvider>
+                </ThemeProvider>
+            </WalletContextProvider>
+        </AppContextProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
