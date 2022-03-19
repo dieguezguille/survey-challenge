4347099a6e1db27a3d53597cb022a2981ef46098
Guillermo Diéguez <guillermodieguez@outlook.com>
2022-03-19 00:03:08 -0300

    ✨ add wallet connector component

--- src/components/common/appbar-menu/appbar-menu.tsx
+++ src/components/common/appbar-menu/appbar-menu.tsx
@@ -2,8 +2,8 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import WalletConnector from '../wallet-connector/wallet-connector';
 
 const AppBarMenu: React.FC = () => (
     <Box sx={{ flexGrow: 1 }}>
@@ -17,7 +17,7 @@ const AppBarMenu: React.FC = () => (
                     >
                         Quiz Challenge
                     </Typography>
-                    <Button color="inherit">Connect Metamask</Button>
+                    <WalletConnector />
                 </Toolbar>
             </Container>
         </AppBar>
---
+++ src/components/common/wallet-connector/wallet-connector.tsx
@@ -0,0 +1,7 @@
+import Button from '@mui/material/Button';
+
+const WalletConnector: React.FC = () => {
+    return <Button color="inherit">Connect Metamask</Button>;
+};
+
+export default WalletConnector;
