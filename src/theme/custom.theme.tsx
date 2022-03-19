9638f170c680ed5f01f48689c256e11f2b317436
Guillermo Diéguez <guillermodieguez@outlook.com>
2022-03-19 20:28:33 -0300

    🎨 improve theming on mui components

--- src/theme/custom.theme.tsx
+++ src/theme/custom.theme.tsx
@@ -1,8 +1,14 @@
 import { createTheme, responsiveFontSizes } from '@mui/material';
+import { MuiContainerStyleOverrides } from './overrides/mui-container.override';
 
 const CustomTheme = createTheme({
     palette: {
         mode: 'dark',
+    },
+    components: {
+        MuiContainer: {
+            styleOverrides: MuiContainerStyleOverrides,
+        },
     },
 });
 
---
+++ src/theme/overrides/mui-container.override.ts
@@ -0,0 +1,8 @@
+import { ComponentsOverrides, Theme } from '@mui/material/styles';
+
+export const MuiContainerStyleOverrides: ComponentsOverrides<Theme>['MuiContainer'] =
+    {
+        root: {
+            padding: '20px',
+        },
+    };
