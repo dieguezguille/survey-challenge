5fe61dbc8fceb560fc1315b1d1363fd3cc2cd30c
Guillermo Agustín Diéguez <guillermodieguez@outlook.com>
2022-03-20 20:06:48 -0300

    🎨 improve overview component

--- src/components/views/overview/overview.view.tsx
+++ src/components/views/overview/overview.view.tsx
@@ -1,9 +1,31 @@
+import { Typography } from '@mui/material';
 import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import { useContext } from 'react';
+import { SurveyContext } from '../../context/survey.context';
 
 const OverviewView: React.FC = () => {
+    const { survey } = useContext(SurveyContext);
     return (
         <Container>
             <h2>Overview</h2>
+            <Stack alignItems="center">
+                <h2>
+                    <i>&apos;{survey?.title}&apos;</i>
+                </h2>
+            </Stack>
+            <Stack>
+                {survey?.questions.map((question, index) => (
+                    <Stack key={index}>
+                        <Typography>{question.text}</Typography>
+                        {/* <ul>
+                            {question.answers.map((answer, index) => (
+                                <li key={index}>{answer}</li>
+                            ))}
+                        </ul> */}
+                    </Stack>
+                ))}
+            </Stack>
         </Container>
     );
 };
