b11cefa2274c50d3d82c8d05f0527c308e982881
Guillermo Diéguez <guillermodieguez@outlook.com>
2022-03-19 23:00:27 -0300

    🚧 wip countdown timer

--- src/components/common/question/question.tsx
+++ src/components/common/question/question.tsx
@@ -4,6 +4,8 @@ import Image from 'mui-image';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import { SurveyImageWrapper } from '../../styled/survey-image-wrapper/survey-image-wrapper';
+import { LinearProgress, Typography } from '@mui/material';
+import { useCallback, useEffect, useState } from 'react';
 
 type QuestionProps = {
     question: ISurveyQuestion;
@@ -11,14 +13,45 @@ type QuestionProps = {
 };
 
 const Question: React.FC<QuestionProps> = ({ question, onNextQuestion }) => {
+    const [remainingTime, setRemainingTime] = useState<number>(
+        question.lifetimeSeconds
+    );
+
+    const [progress, setProgress] = useState<number>(0);
+    const [intervalId, setIntervalId] = useState<number>();
+
+    const handleTimeout = useCallback(() => {
+        // onNextQuestion();
+    }, []);
+
     const handleOptionClick = () => {
-        // TODO: Save answer
-        // Request next question
+        if (intervalId) clearInterval(intervalId);
         onNextQuestion();
     };
 
+    useEffect(() => {
+        const timer = window.setInterval(() => {
+            setProgress((oldProgress) => {
+                if (oldProgress === 100) {
+                    window.clearInterval(timer);
+                    handleTimeout();
+                    return 100;
+                }
+                const diff = 100 / question.lifetimeSeconds;
+                return Math.min(oldProgress + diff, 100);
+            });
+            setRemainingTime((oldRemainingTime) =>
+                oldRemainingTime > 0 ? oldRemainingTime - 1 : 0
+            );
+        }, 1000);
+        setIntervalId(timer);
+        return () => {
+            window.clearInterval(timer);
+        };
+    }, [handleTimeout, question.lifetimeSeconds]);
+
     return (
-        <Stack spacing={6}>
+        <Stack spacing={6} padding="3em">
             <Stack alignItems="center" spacing={4}>
                 <h2>
                     <i>{question.text}</i>
@@ -26,7 +59,16 @@ const Question: React.FC<QuestionProps> = ({ question, onNextQuestion }) => {
                 <SurveyImageWrapper>
                     <Image src={question.image} showLoading />
                 </SurveyImageWrapper>
-                <div>Remaining time: {question.lifetimeSeconds} seconds</div>
+                <Stack spacing={4} alignSelf="start" width="100%">
+                    <Typography variant="body2">
+                        Remaining time: <b>{remainingTime}</b> seconds...
+                    </Typography>
+                    <LinearProgress
+                        sx={{ width: '100%' }}
+                        variant="determinate"
+                        value={progress}
+                    />
+                </Stack>
             </Stack>
             <Stack>
                 <Grid
