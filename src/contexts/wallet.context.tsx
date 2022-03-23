3d7d035711f913b9340bd119fee837cfe089c07d
Guillermo Agustín Diéguez <guillermodieguez@outlook.com>
2022-03-23 10:04:52 -0300

    🎨 remove console log

--- src/contexts/wallet.context.tsx
+++ src/contexts/wallet.context.tsx
@@ -153,7 +153,6 @@ const WalletProvider: React.FC = ({ children }) => {
     }, [enqueueSnackbar, isMetamaskError, provider, setIsLoading]);
 
     const connect = useCallback(async () => {
-        console.log('connecting');
         setIsLoading(true);
         try {
             if (provider) {
